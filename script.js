c246163c6daa13f25f6a20a1dd5e0af496585339
Pritam Kumar <developerpritam@gmail.com>
2021-06-01 20:16:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var x = document.getElementById('mainc').innerHTML;
+for (let index = 0; index < 10; index++) {
+    document.getElementById('mainc').innerHTML = document.getElementById('mainc').innerHTML + x;
+
+}
